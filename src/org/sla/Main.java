71743a2ef2d5aff4ea13fa77fb9e9ed0c29d1c68
vhernandez <vhernandez@scienceleadership.org>
2018-09-13 09:30:53 -0400

    Initial commit

---
+++ src/org/sla/Main.java
@@ -0,0 +1,9 @@
+package org.sla;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Mr. Hernandez: Who's here?");
+    }
+}
