282b731bf63f9ec21a438455e0c9004abedebcb0
andrewrodebaugh <aquahydration@gmail.com>
2018-09-14 13:18:32 -0400

    Tab things

--- src/org/sla/Main.java
+++ src/org/sla/Main.java
@@ -7,8 +7,8 @@ public class Main {
         System.out.println("Mr. Hernandez: Who's here on Friday");
         System.out.println("Antonio: I am here!");
         System.out.println("Andrew is here.");
-	System.out.println("Alan: I'm here.");
-	System.out.println("Eric is here");
+	    System.out.println("Alan: I'm here.");
+	    System.out.println("Eric is here");
         System.out.println("Paxton Wentzell: I'm here!!");
     }
 }
