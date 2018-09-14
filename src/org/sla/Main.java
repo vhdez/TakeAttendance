51a6358dec7e0be97440316076e9803232ab4fc0
vhernandez <vhernandez@scienceleadership.org>
2018-09-14 12:59:02 -0400

    Specified Friday

--- src/org/sla/Main.java
+++ src/org/sla/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Mr. Hernandez: Who's here?");
+        System.out.println("Mr. Hernandez: Who's here on Friday");
     }
 }
