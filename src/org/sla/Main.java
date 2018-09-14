9f18b0c5863cda5ddcea0e43723db5a00004f1d9
ADeRock <aderock19@scienceleadership.org>
2018-09-14 13:06:50 -0400

    Added my name

--- src/org/sla/Main.java
+++ src/org/sla/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Mr. Hernandez: Who's here on Friday");
+        Systen.out.println("Antonio: I am here!");
     }
 }
